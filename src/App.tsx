dbed0d08c94d32e843ab05b7e0ab939d9a431e6a
soar1219 <tsubasa1219.info@gmail.com>
2023-10-12 01:20:21 +0900

    グローバルスタイルの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Button, ChakraProvider } from '@chakra-ui/react';
 import './App.css';
+import theme from './theme/theme';
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+    <ChakraProvider theme={theme}>
+      <Button colorScheme='teal'>ボタン</Button>
+      <p>ああああああ</p>
+    </ChakraProvider>
   );
 }
 
