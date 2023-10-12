12d38976351fa39b5677d63c2af4f9323c4365cd
soar1219 <tsubasa1219.info@gmail.com>
2023-10-13 00:14:36 +0900

    ログイン画面の作成

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,9 +1,19 @@
 import { FC, memo } from "react";
+import { Box, Button, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 
 export const Login: FC = memo(() => {
     return(
-        <div>
-            <p>ログインページです</p>
-        </div>
+        <Flex align="center" justify="center" height="100vh">
+            <Box backgroundColor="white" width="sm" padding={4} borderRadius="md" shadow="md">
+                <Heading as="h1" size="lg" textAlign="center">
+                    ユーザー管理アプリ
+                </Heading>
+                <Divider my={4} />
+                <Stack spacing={6} py={4} px={10}>
+                    <Input placeholder="ユーザーID" />
+                    <Button backgroundColor="teal.400" color="white" _hover={{opacity:"0.8"}}>ログイン</Button>
+                </Stack>
+            </Box>
+        </Flex>
     )
 })
