531195bd06e3c893f1e4eb383881777b11ec40b8
soar1219 <tsubasa1219.info@gmail.com>
2023-10-12 20:49:30 +0900

    画面遷移の実装

--- src/components/molecures/MenueDrawer.tsx
+++ src/components/molecures/MenueDrawer.tsx
@@ -1,5 +1,6 @@
 import { FC, memo } from "react";
 import { Drawer, DrawerOverlay, DrawerContent, DrawerBody, Button } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
     onClose: () => void;
@@ -9,14 +10,31 @@ type Props = {
 export const MenueDrawer: FC<Props> = memo((props) => {
 
     const { onClose , isOpen } = props;
+    const history = useNavigate();
+
+    const handleHomeClick = () => {
+        history("/home");
+        onClose(); 
+    };
+
+    const handleUserManagementClick = () => {
+        history("/home/user-management");
+        onClose(); 
+    };
+
+    const handleSettingClick = () => {
+        history("/setting");
+        onClose(); 
+    };
+
     return(
         <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
             <DrawerOverlay>
                 <DrawerContent>
                     <DrawerBody p={0} bg="gray.100" >
-                        <Button w="100%">TOP</Button>
-                        <Button w="100%">ユーザー一覧</Button>
-                        <Button w="100%">設定</Button>
+                        <Button onClick={handleHomeClick} w="100%">TOP</Button>
+                        <Button onClick={handleUserManagementClick} w="100%">ユーザー一覧</Button>
+                        <Button onClick={handleSettingClick} w="100%">設定</Button>
                     </DrawerBody>
                 </DrawerContent>
             </DrawerOverlay>
--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -2,10 +2,14 @@ import { FC, memo } from "react";
 import { Flex, Heading, Link, Box, useDisclosure } from "@chakra-ui/react";
 import { MenueIconButton } from "../../atoms/button/MenueIconButton";
 import { MenueDrawer } from "../../molecures/MenueDrawer";
+import { useNavigate } from "react-router-dom";
 
 
 export const Header: FC = memo(() => {
     const { isOpen, onClose, onOpen } = useDisclosure();
+
+    const history = useNavigate();
+
     return(
         <>
             <Flex as="nav"
@@ -15,14 +19,14 @@ export const Header: FC = memo(() => {
                 justify="space-between"
                 padding={{base: 3, md: 5}}
                 >
-                <Flex align="center" as="a" mr={8} _hover={{cursor: "pointer"}}>
+                <Flex align="center" as="a" mr={8} _hover={{cursor: "pointer"}} onClick={() => history("/home")}>
                     <Heading as="h1" fontSize={{base: "md" , md:"lg"}}>ユーザー管理アプリ</Heading>
                 </Flex>
                 <Flex align="center" fontSize="sm" flexGrow={2} display={{base: "none" ,md: "flex"}}>
                     <Box pr={4}>
-                        <Link>ユーザー一覧</Link>
+                        <Link onClick={() => history("/home/user-manegemant")}>ユーザー一覧</Link>
                     </Box>
-                    <Link>設定</Link>
+                    <Link onClick={() => history("/setting")}>設定</Link>
                 </Flex>
                 <MenueIconButton onOpen={onOpen} />
                 <MenueDrawer isOpen={isOpen} onClose={onClose} />
