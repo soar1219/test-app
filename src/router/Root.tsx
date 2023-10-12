998c02eb5e06f1a9d3af21f83dfbfc44e3415c1e
soar1219 <tsubasa1219.info@gmail.com>
2023-10-12 11:57:26 +0900

    ヘッダーの作成

--- src/router/Root.tsx
+++ src/router/Root.tsx
@@ -5,13 +5,14 @@ import { Home } from "../components/pages/Home";
 import { UserManegement } from "../components/pages/UserManegement";
 import { Setting } from "../components/pages/Setting";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 
 export const Root: FC = memo(() => {
     return (
         <Routes>
             <Route path="/" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/home/user-manegemant" element={<UserManegement />} />
+            <Route path="/home" element={<HeaderLayout><Home /></HeaderLayout>} />
+            <Route path="/home/user-manegemant" element={<HeaderLayout><UserManegement /></HeaderLayout>} />
             <Route path="/setting" element={<Setting />} />
             <Route path='/*' element={<Page404/>} />
         </Routes>
