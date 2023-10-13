16074fbb00c8d2976a3fad9331021666224f0e66
soar1219 <tsubasa1219.info@gmail.com>
2023-10-13 10:53:23 +0900

    メッセージの表示機能実装

--- src/hooks/api/useAuth.ts
+++ src/hooks/api/useAuth.ts
@@ -2,22 +2,25 @@ import { useCallback, useState } from "react"
 import axios from "axios"
 import { User } from "../../types/spi/user"
 import { useNavigate } from "react-router-dom";
+import { useMessage } from "../useMessage";
 
 
 
 export const useAuth = () => {
     const history = useNavigate();
+    const { shoeMessage } = useMessage();
     const [loading, setLoading] = useState(false);
 
     const login = useCallback((id: string) => { 
         setLoading(true)
         axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
             if(res.data) {
+                shoeMessage({ title: "ログインしました", status:"success"})
                 history("/home")
             }else {
-                alert("ユーザーが見つかりません")
+                shoeMessage({ title: "ユーザーが見つかりません", status:"error"})
             }
-        }).catch(() => alert("ログインできません")).finally(() => {setLoading(false)})
-    }, [ history])
+        }).catch(() => shoeMessage({ title: "ログインできません", status:"error"})).finally(() => {setLoading(false)})
+    }, [ history, shoeMessage])
     return { login , loading} 
 }
