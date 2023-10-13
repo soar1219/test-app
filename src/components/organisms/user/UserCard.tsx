c9d68b0f56308755d488c22ccb0d7b4fb3412702
soar1219 <tsubasa1219.info@gmail.com>
2023-10-13 12:49:17 +0900

    ユーザー一覧画面の作成

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,23 @@
+import { FC, memo } from "react";
+import { Box, Stack, Image, Text } from "@chakra-ui/react";
+
+type Props = {
+    imageUrl: string;
+    useNname:string;
+    fullName:string;
+}
+
+export const UserCard: FC<Props> = memo((props) => {
+    const { imageUrl, useNname, fullName } = props;
+    return(
+        <>
+           <Box w="260px" height="260px" bg="white" borderRadius="10px" shadow="md" p={4} _hover={{opacity:"0.8", cursor:"pointer"}}>
+                <Stack textAlign="center">
+                    <Image m="auto" alt="プロフィール画像" borderRadius="50%" boxSize="160px" src={imageUrl} />
+                    <Text fontSize="lg" fontWeight="bold">{useNname}</Text>
+                    <Text fontSize="sm" color="gray">{fullName}</Text>
+                </Stack>
+            </Box>
+        </>
+    )
+})
